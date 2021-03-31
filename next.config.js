9812837d430174a0770dcc039aafde625d869a72
omaralsoudanii <omaralsoudani@gmail.com>
2021-03-31 19:52:22 +0300

    test preact next module

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
+// const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//   enabled: process.env.ANALYZE === 'true',
+// })
+const withPreact = require('next-plugin-preact')
 
-module.exports = withBundleAnalyzer({
+module.exports = withPreact({
   poweredByHeader: false,
   future: {
     webpack5: true,
