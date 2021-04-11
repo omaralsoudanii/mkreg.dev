f6da88ac76e07020796057c4dd85afd9d599854a
omaralsoudanii <omaralsoudani@gmail.com>
2021-04-11 13:52:55 +0300

    bump vers

--- src/components/Layouts/index.tsx
+++ src/components/Layouts/index.tsx
@@ -5,7 +5,7 @@ import { CenteredColumn } from '../CenteredColumn'
 
 export function Main({ children }) {
   return (
-    <main className="h-screen max-w-screen-md mx-auto justify-items-center">
+    <main className="w-10/12 h-screen mx-auto md:w-8/12 lg:w-1/2 justify-items-center">
       <Header />
       <Page>
         <CenteredColumn>{children}</CenteredColumn>
