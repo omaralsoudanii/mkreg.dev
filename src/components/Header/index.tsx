bc1aae49fea4bf2ed6e09070d14a35109dbafa54
omaralsoudanii <omaralsoudani@gmail.com>
2021-04-13 22:35:32 +0300

    header max-w

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -141,7 +141,7 @@ export default function Header() {
           })}
       </div>
 
-      <div className="hidden max-w-screen-sm grid-cols-5 gap-4  mx-auto sm:grid">
+      <div className="hidden grid-cols-5 gap-4 mx-auto max-w-prose sm:grid">
         {RoutesMetadata.map((route) => {
           const navClass =
             route.href === router.pathname ? 'nav-link active' : 'nav-link'
