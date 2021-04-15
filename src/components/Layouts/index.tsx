3a33aa0debea795a2e64dc1cc0233e091233ebae
omaralsoudanii <omaralsoudani@gmail.com>
2021-04-15 17:02:54 +0300

    tweak font

--- src/components/Layouts/index.tsx
+++ src/components/Layouts/index.tsx
@@ -3,7 +3,7 @@ import { Footer } from '@/components/Footer'
 
 export function Main({ children }) {
   return (
-    <main className="px-4 pt-24 pb-16 h-screen mx-auto mt-6 max-w-prose sm:px-0">
+    <main className="px-4 pt-24 pb-16 mx-auto mt-6 max-w-prose sm:px-0">
       <Header />
       {children}
       <Footer />
