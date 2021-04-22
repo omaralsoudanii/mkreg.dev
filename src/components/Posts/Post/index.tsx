476d6eb90da3e6ba0a1ddc80022ef7cc928222ff
omaralsoudanii <omaralsoudani@gmail.com>
2021-04-22 14:09:32 +0300

    upgrade haproxy

--- src/components/Posts/Post/index.tsx
+++ src/components/Posts/Post/index.tsx
@@ -20,9 +20,7 @@ export default function PostContainer({ children, frontMatter }) {
   const Meta = () => (
     <div className="grid grid-cols-1 mt-1 mb-8 sm:grid-cols-1">
       <p className="px-2 !mb-1 text-base font-medium text-mk-darkest dark:text-mk-lighter">
-        {`Published on ${dayjs(new Date(frontMatter.publishedAt)).format(
-          'MMMM,DD YYYY'
-        )}`}
+        {`${dayjs(new Date(frontMatter.publishedAt)).format('MMMM DD, YYYY')}`}
       </p>
       <div className="flex flex-wrap">
         {frontMatter.tags.map((t: string) => (
