30748f690d9fb07d4d5206f630068f578b22c30b
omaralsoudanii <omaralsoudani@gmail.com>
2021-05-03 11:17:12 +0300

    header size fix

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -149,7 +149,7 @@ export default function Header() {
           })}
       </div>
 
-      <div className="hidden max-w-screen-sm lg:max-w-screen-md grid-cols-4 gap-4 mx-auto md:grid">
+      <div className="hidden max-w-screen-sm grid-cols-4 gap-4 mx-auto md:grid">
         {RoutesMetadata.map((route) => {
           const navClass =
             route.href === router.pathname
