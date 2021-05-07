aef2dec4b4a96d7115150a1efbd68399606d222d
omaralsoudanii <omaralsoudani@gmail.com>
2021-05-07 20:41:22 +0300

    nope, still bad

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -52,13 +52,6 @@ export default class MyDocument extends Document {
           <meta name="msapplication-TileColor" content="#2d89ef" />
           <meta name="msapplication-config" content="/meta/browserconfig.xml" />
           <meta name="theme-color" content="#050505" />
-          <link
-            rel="preload"
-            href="/static/fonts/Inter-latin-15.var.woff2"
-            as="font"
-            type="font/woff2"
-            crossOrigin="anonymous"
-          />
         </Head>
         <body>
           <Main />
