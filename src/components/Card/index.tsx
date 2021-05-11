53f5ea1278128cea1906a32c004644d3ef910fca
omaralsoudanii <omaralsoudani@gmail.com>
2021-05-11 23:56:07 +0300

    adjust links

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -2,7 +2,7 @@ import * as Icons from '@/components/Icons'
 
 const Card = ({ title, url, icon, desc }) => (
   <a key={title} href={url} target="_blank" rel="noopener noreferrer">
-    <div className="flex items-center p-2 my-8 border border-gray-400 rounded md:p-4 dark:border-gray-700">
+    <div className="flex items-center p-2 my-8 border border-gray-400 rounded md:px-4 md:py-4 dark:border-gray-700">
       <div className="w-12 h-12 ml-0 mr-4">
         <span className="sr-only">{title}</span>
         {Icons[icon]({
@@ -13,7 +13,7 @@ const Card = ({ title, url, icon, desc }) => (
         <h3 className="!mt-2 !mb-1 !text-lg md:!text-xl !leading-normal">
           {title}
         </h3>
-        <p className="!mt-1 !mb-2  !font-normal md:!text-lg !leading-normal text-secondary clamp-5">
+        <p className="!mt-1 !mb-2  !font-normal !leading-normal text-secondary clamp-5">
           {desc}
         </p>
       </div>
