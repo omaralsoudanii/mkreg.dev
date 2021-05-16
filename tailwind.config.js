039fcafd8d9efa151e557b873b57b814c2586f59
omaralsoudanii <omaralsoudani@gmail.com>
2021-05-16 14:58:59 +0300

    test var font

--- tailwind.config.js
+++ tailwind.config.js
@@ -36,7 +36,7 @@ module.exports = {
         'monospace',
       ],
       display:
-        '"HK Grotesk", Inter, ui-sans-serif, Roboto, "Helvetica Neue", sans-serif',
+        'Raleway, Inter, ui-sans-serif, Roboto, "Helvetica Neue", sans-serif',
       sans: 'Inter, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
     },
     extend: {
