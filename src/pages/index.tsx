8febf6e33abab334022defbcb1fa81621cb85e43
omaralsoudanii <omaralsoudani@gmail.com>
2021-05-18 14:45:24 +0300

    heights and salt

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,13 +15,13 @@ const Home = () => {
       <Container>
         <section>
           <div className="!mb-10 md:!mb-24">
-            <h1 className="!font-semibold">Omar Alsoudani</h1>
+            <h1 className="!font-semibold !mb-4">Omar Alsoudani</h1>
             <h2 className="sub-heading">Software Engineer</h2>
             <h2 className="sub-heading">Architect</h2>
             <h2 className="sub-heading">Creator</h2>
           </div>
 
-          <div className="flex flex-col md:!mb-24 !mb-10 items-start justify-start divide-y-2 divide-gray-700 dark:divide-gray-400 md:justify-center md:items-center md:divide-y-0 md:flex-row md:space-x-6 md:mt-16">
+          <div className="flex flex-col md:!mb-16 !mb-10 items-start justify-start divide-y-2 divide-gray-700 dark:divide-gray-400 md:justify-center md:items-center md:divide-y-0 md:flex-row md:space-x-6 md:mt-16">
             <div className="pt-6 space-x-2 space-y-5 md:pb-8">
               <h1 className="!px-2 !font-semibold !mb-4 !mt-4 md:!mb-4 md:!mt-4 md:!leading-[10rem] md:!px-6 md:border-gray-700 md:dark:border-gray-300  md:!border-r-2">
                 Purpose
