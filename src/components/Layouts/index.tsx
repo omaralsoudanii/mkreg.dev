f76372809bdcc94ae1195bd04ffa569255eb6d7c
omaralsoudanii <omaralsoudani@gmail.com>
2021-06-08 17:04:19 +0300

    base

--- src/components/Layouts/index.tsx
+++ src/components/Layouts/index.tsx
@@ -3,7 +3,7 @@ import { Footer } from '@/components/Footer'
 
 export function Main({ children }) {
   return (
-    <div className="bg-light dark:bg-dark">
+    <div>
       <Header />
       <main className="flex flex-col justify-center pt-16 pb-8 mt-12 lg:pt-12 lg:mt-4 bg-light dark:bg-dark">
         {children}
