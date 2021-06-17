27adc0d18ec86d5f7ee3a839342f2ff963de4e36
omaralsoudanii <omaralsoudani@gmail.com>
2021-06-18 02:19:46 +0300

    next v11 testing / refactor structuring (WIP)

--- src/components/MDXComponents/index.tsx
+++ src/components/MDXComponents/index.tsx
@@ -1,11 +1,11 @@
 import NextLink from '@/components/NextLink'
-import dynamic from 'next/dynamic'
 import NextImage from '@/components/NextImage'
 import CopyCode from '@/components/CopyCode'
+import Card from '@/components/Card'
 
 const MDXComponents = {
   a: NextLink,
-  Card: dynamic(() => import('../Card')),
+  Card: Card,
   Image: NextImage,
   pre: CopyCode,
 }
