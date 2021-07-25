8d08990ab1450318d9ec73da814d6e102e7c9fb8
omaralsoudanii <omaralsoudani@gmail.com>
2021-07-25 03:45:31 +0300

    fix skip-nav layout shift

--- src/components/LayoutWrapper/index.tsx
+++ src/components/LayoutWrapper/index.tsx
@@ -5,11 +5,11 @@ import SectionContainer from '@/components/SectionContainer'
 const LayoutWrapper = ({ children }) => {
   return (
     <SectionContainer>
+      <a href="#skip" className="skip-content-nav">
+        Skip to content
+      </a>
       <div className="flex flex-col justify-between min-h-screen">
         <header className="sm:flex py-12 mb-6 sm:items-center sm:justify-end">
-          <a href="#skip" className="skip-content-nav">
-            Skip to content
-          </a>
           <Header />
         </header>
         <main className="mb-auto">{children}</main>
