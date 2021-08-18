47769c212540eec9ebb0ace8b3e99a5eac52654d
omaralsoudanii <omaralsoudani@gmail.com>
2021-08-18 19:29:49 +0300

    list adjustments

--- src/components/Layouts/PostsList.tsx
+++ src/components/Layouts/PostsList.tsx
@@ -21,7 +21,7 @@ const Row = ({ href, frontMatter }) => {
       <a className="inline-block my-4 py-2 md:my-6 w-full">
         <div className="w-full">
           <div className="flex flex-col justify-between md:flex-row md:items-center">
-            <h2 className="!text-xl md:!text-2xl !mb-0 w-full !mt-0 text-display">
+            <h2 className="!text-xl md:!text-2xl !font-semibold !tracking-normal !mb-0 w-full !mt-0 text-display">
               {frontMatter.title}
             </h2>
             <p className="w-56 !font-medium !text-base md:!text-lg !pt-1 md:!pt-0 text-left text-tertiary md:text-right !my-0">
