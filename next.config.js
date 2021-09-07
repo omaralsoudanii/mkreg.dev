9da99bdfd58e6cac7e84e00ca780217bba87ba97
omaralsoudanii <omaralsoudani@gmail.com>
2021-09-07 17:57:12 +0000

    feat:
    - tweak nginx cache behaviour
    - generate etag => on
    - tweak general nginx configs
    - add mime.types
    - compression add better mime.types
    - test cache without Cache-Control, just etag

--- next.config.js
+++ next.config.js
@@ -7,7 +7,6 @@ module.exports = {
     minimumCacheTTL: 604800, // 1 week, we can make it a month to sync with CF, but I wanna try 1 week first
   },
   compress: process.env.compress === 'true',
-  generateEtags: false,
   webpack: (config, { dev, isServer }) => {
     config.module.rules.push({
       test: /\.svg$/,
